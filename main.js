f723485749fc4e66036201f50f01e9fe1fcb6577
Emre KARA <emre.kara5@ogr.sakarya.edu.tr>
2018-08-16 11:31:10 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const API_URL = "https://free.currencyconverterapi.com/api/v6/convert?q=USD_TRY,EUR_TRY&compact=y";
+
+function getCurrencies() {
+    return new Promise((resolve, reject) => {
+        var request = new XMLHttpRequest();
+        request.open('GET', API_URL, true);
+        request.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                const response = JSON.parse(request.response);
+                resolve(response);
+
+            } else {
+                reject(request);
+            }
+        };
+        request.onerror = function (r) {
+            reject(r);
+        };
+        request.send();
+    });
+
+}
+function setCurrenciesText() {
+    getCurrencies().then(currencies => {
+        const usdToTry= document.getElementById("usd-to-try");
+        const eurToTry= document.getElementById("eur-to-try");
+        usdToTry.innerHTML = `1 $ = ${currencies.USD_TRY.val} ₺`;
+        eurToTry.innerHTML = `1 € = ${currencies.EUR_TRY.val} ₺`;
+
+        console.log(currencies);
+    });
+
+}
+window.addEventListener("load",()=>setCurrenciesText());
