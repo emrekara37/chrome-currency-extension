98dcc6ae40e274b0a7304af1ed86e8ea9f052d89
Emre KARA <emre.kara5@ogr.sakarya.edu.tr>
2018-08-18 21:32:30 +0300

    Api changed to dovizcom

--- main.js
+++ main.js
@@ -1,29 +1,64 @@
-const API_URL = "https://free.currencyconverterapi.com/api/v6/convert?q=USD_TRY,EUR_TRY&compact=y";
 var txtDollarValue;
 var txtEuroValue;
 var apiDollarValue;
 var apiEuroValue;
-function getCurrencies() {
-    fetch(API_URL, { method: 'GET', mode: 'cors' }).then(res => res.json()).then((res) => {
-        apiDollarValue = res.USD_TRY.val;
-        apiEuroValue = res.EUR_TRY.val;
-        txtDollarValue = parseInt(document.getElementById("txtDollar").value);
-        txtEuroValue = parseInt(document.getElementById("txtEuro").value);
-        setDollarText();
-        setEuroText();
+var dollarPositive = false;
+var euroPositive = false;
+function getCurrency(currencyCode) {
+
+    return new Promise(function (resolve, reject) {
+       const url = `https://www.doviz.com/api/v1/currencies/${currencyCode}/latest`;
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        request.setRequestHeader("Access-Control-Allow-Origin","*");
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                const resp = request.response;
+                resolve(JSON.parse(resp));
+            } else {
+                reject(request);
+            }
+        };
+        request.onerror = function (r) {
+            reject(r);
+        };
+        request.send();
     });
 }
+function getCurrencies() {
+    
+
+}
 function setDollarText() {
+ 
     const usdToTry = document.getElementById("usdTryValue");
     usdToTry.innerHTML = txtDollarValue * apiDollarValue;
+    const icon= `<i class="fa fa-caret-${dollarPositive ? "up up": "down down"}" style="margin-bottom:-5px;"></i>`;
+    document.getElementById("icond").innerHTML =icon;
 }
 function setEuroText() {
+   
     const eurToTry = document.getElementById("euroTryValue");
-    eurToTry.innerHTML = txtEuroValue * apiEuroValue;
+    const icon= `<i class="fa fa-caret-${euroPositive ? "up up": "down down"}" style="margin-bottom:-5px;"></i>`;
+    eurToTry.innerHTML = txtEuroValue * apiEuroValue; 
+    document.getElementById("icone").innerHTML =icon; 
 }
 
 window.addEventListener("load", () => {
-    getCurrencies();
+    getCurrency("USD").then((res=>{
+        txtDollarValue = parseInt(document.getElementById("txtDollar").value);
+        apiDollarValue= res.selling;
+        dollarPositive= res.change_rate > 0;
+        setDollarText();
+    }));
+    getCurrency("EUR").then((res=>{
+        txtEuroValue = parseInt(document.getElementById("txtDollar").value);
+        apiEuroValue= res.selling;
+        euroPositive = res.change_rate > 0;
+        setEuroText();
+    }));
+    
     document.getElementById("txtDollar").addEventListener("keyup", (e) => {
         if (e.target.value) {
             txtDollarValue = parseInt(e.target.value);
@@ -36,14 +71,5 @@ window.addEventListener("load", () => {
             setEuroText();
         }
     });
-
-
-  /**
-   * 
-   * New Feature next release version
-   
-    setTimeout(() => {
-        getCurrencies();
-    }, 3000);
-    */
+  
 });
